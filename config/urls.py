5cf793d7981242955a08b11f30916ec1a6fc07e9
Asilbek Axatov <sizning_emailingiz@gmail.com>
2026-08-12 18:34:12 +0500

    first push

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectaular.views import (
+from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView
